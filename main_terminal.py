05ecd4349bd5c997ec7cd2ef9da4f255b59c65c7
ireero <78822225+ireero@users.noreply.github.com>
2023-10-28 20:50:02 -0300

    Adicionando tratativa de leitura de textos em imagens e adicionando tratattiva para deletar arquivos.

--- main_terminal.py
+++ main_terminal.py
@@ -1,10 +1,13 @@
 from leitor_de_pastas import LeitorDePastas
 from tratativa_de_zipagem import TratativaDeZipagem
+from tratativa_de_imagens import TratativaDeImagens
+import os
+
 
 continua = True
 
 def escolha():
-    print('Ações disponíveis: \n1 - Leitura de arquivo\n2 - Zipagem de arquivo\n3 - Substituição de texto\n4 - Leitura de texto em imagens')
+    print('Ações disponíveis: \n1 - Leitura de arquivo\n2 - Zipagem de arquivo\n3 - Substituição de texto\n4 - Leitura de texto em imagens\n5 - Deletar arquivo')
     escolha = input('Digite a opção que você deseja: ')
     if escolha == '1':
         l = LeitorDePastas()
@@ -13,7 +16,15 @@ def escolha():
         return continuar
     elif escolha == '2':
         z = TratativaDeZipagem()
-        
+    elif escolha == '4':
+        i = TratativaDeImagens()
+    elif escolha == '5':
+        l = LeitorDePastas()
+        for index,arquivo in enumerate(l.pastas, start=1):
+            print(index,' - ',arquivo)
+        index = int(input('Arquivo que deseja apagar: ')) - 1
+        os.remove(l.pastas[index])
+        print('Pronto! arquivo removido com sucesso')
     else:
         print('Ação não implementada')
         return 's'
