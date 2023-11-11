99bbc6c598d1df744a62b10811799d3764a2017a
ireero <78822225+ireero@users.noreply.github.com>
2023-11-10 21:20:56 -0300

    template de index adicionado.

--- calls.py
+++ calls.py
@@ -1,3 +1,7 @@
 from flask import Blueprint, render_template, request, send_file, jsonify
 
 calls = Blueprint('calls',__name__)
+
+@calls.route('/')
+def index():
+    return render_template('index.html')
